ea39980c37212e6a43e2f0ee84d72b1d8a3612d4
Lugolu <lugolu@gmail.com>
2020-09-12 00:53:28 -0300

    by range

--- api_routes/api.js
+++ api_routes/api.js
@@ -4,8 +4,8 @@ const api = require('../services/api.js');
 
 const constants = require('../common/constants.js');
 
-router.route('/country/:country?').get(async function (req, res, next) {
-	let v = await api.country(req.params.country);
+router.route('/country/:country/:fechaDesde/:fechaHasta').get(async function (req, res, next) {
+	let v = await api.country(req.params.country, req.params.fechaDesde, req.params.fechaHasta);
 	res.status(constants.HTTP_OK).json(v);
 });
 
--- services/api.js
+++ services/api.js
@@ -6,30 +6,22 @@ const { FECHA } = require('../generated.json');
 
 const axios = require('axios');
 
-async function country(country) {
+async function country(country, fechaDesde, fechaHasta) {
 	let respuesta = []
 
-	for (let month = 3; month < 10; month ++) {
-		for (let day = 1; day < 31; day ++) {
-			if ((month == 4 || month == 6) && day > 30) {
-				continue
-			}
-
-			let fecha = '2020-' + lpad(month) +'-' + lpad(day) +''
-			let url = 'https://api.covid19tracking.narrativa.com/api/' + fecha + '/country/' + country
-			await axios({
-				method: 'get',
-				url: url
-				}).then((response) => {
-					// Success 🎉
-					respuesta.push(process(response.data))
-				})
-				.catch((error) => {
-					// Error 😨
-					respuesta = { err: error}
-				});
-		}
-	}
+	let url = 'https://api.covid19tracking.narrativa.com/api/country/' + country + '?date_from=' + fechaDesde + '&date_to=' + fechaHasta
+	await axios({
+		method: 'get',
+		url: url
+		}).then((response) => {
+			// Success 🎉
+			respuesta = (process(response.data))
+		})
+		.catch((error) => {
+			// Error 😨
+			console.error(error)
+			respuesta = { err: error}
+		});
 
 	return respuesta
 }
@@ -44,42 +36,52 @@ function lpad (str) {
 	return tmp
 }
 
-function process (response) {
+function process (res) {
+	let response = []
 	//console.log(response.dates)
-	let dates = Object.values(response.dates)
-	//console.log(countries[0]);
-	let country = Object.values(dates[0])
-	//console.log(country[0]);
 
-	country = Object.values(country[0])
-	//console.log(country[0]);
+	let dates = Object.values(res.dates)
+	for (let d in dates) {
+		let date = dates[d]
+		//console.error(dates[d])
 
-	let fecha = country[0].date
-	console.error(fecha)
-
-	let confirmedGBA = 0
-	let deathsGBA = 0
-	let confirmedInt = 0
-	let deathsInt = 0
-
-	for (let r in country[0].regions) {
-		let region = country[0].regions[r]
-		if (region.id == 'buenos_aires' || region.id == 'ciudad_autonoma_de_buenos_aires') {
-			confirmedGBA += region.today_new_confirmed
-			deathsGBA += region.today_new_deaths
+		let fecha = null
+		let confirmedGBA = 0
+		let deathsGBA = 0
+		let confirmedInt = 0
+		let deathsInt = 0
+	
+		let country = Object.values(date)
+		country = Object.values(country[0])
+	
+		fecha = country[0].date
+	
+		for (let r in country[0].regions) {
+			let region = country[0].regions[r]
+			if (region.id == 'buenos_aires' || region.id == 'ciudad_autonoma_de_buenos_aires') {
+				confirmedGBA += region.today_new_confirmed
+				deathsGBA += region.today_new_deaths
+			}
+			else {
+				confirmedInt += region.today_new_confirmed
+				deathsInt += region.today_new_deaths
+			}
 		}
-		else {
-			confirmedInt += region.today_new_confirmed
-			deathsInt += region.today_new_deaths
-		}
+
+		console.error(fecha+';'+confirmedGBA+';'+deathsGBA+';'+confirmedInt+';'+deathsInt)
+	
+		response.push(
+			 {
+				fecha: fecha,
+				confirmedGBA: confirmedGBA,
+				deathsGBA: deathsGBA,
+				confirmedInt: confirmedInt,
+				deathsInt: deathsInt
+			}
+		);
 	}
-	return {
-		fecha: fecha,
-		confirmedGBA: confirmedGBA,
-		deathsGBA: deathsGBA,
-		confirmedInt: confirmedInt,
-		deathsInt: deathsInt
-	}
+
+	return response
 }
 //today_new_confirmed	283
 //today_new_deaths
